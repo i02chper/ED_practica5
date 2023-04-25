fede818e1dbb143ce404c3a1b7d5e4270fc612a3
Rafael Chacón Pérez <i02chper@uco.es>
2023-04-25 11:24:53 +0200

    Test ip_utils completado

--- dos_detector-open_addressing_2.0/dos_detector-open_addressing/ip_utils.cpp
+++ dos_detector-open_addressing_2.0/dos_detector-open_addressing/ip_utils.cpp
@@ -19,7 +19,8 @@ IpToInt::operator() (const IP& ip) const
     //TODO
     //Remember: casting to 64 bits unsigned integer to do not loss bits.
     //Hint: mult by 2^n means shift to left n bits.
-
+    ret_val=(static_cast<uint64_t>(ip.bytes[0])<<24)+(static_cast<uint64_t>(ip.bytes[1])<<16)+
+        (static_cast<uint64_t>(ip.bytes[2])<<8)+(static_cast<uint64_t>(ip.bytes[3]));
     //
     return ret_val;
 }
@@ -29,7 +30,10 @@ operator<< (std::ostream& out, const IP& ip)
 {
     //TODO
     //Remember: we want to output the ascii code, not the char.
-
+    out << static_cast<int>(ip.bytes[0]) << "."
+        << static_cast<int>(ip.bytes[1]) << "."
+        << static_cast<int>(ip.bytes[2]) << "."
+        << static_cast<int>(ip.bytes[3]);
     //
     return out;
 }
@@ -41,7 +45,34 @@ operator>>(std::istream& in, IP& ip) noexcept(false)
     //Hint: you can use a std::istringstream to convert from text to uint8_t.
     //Hint: you can use the std::replace algorithm to replace '.' by ' '.
     //Remember: if a wrong format is detected, throw an runtime_error exception.
-
+    std::string direction;
+        in >> direction;
+        if(in){
+            for(size_t i=0;i<direction.size();i++){
+                if(direction[i]=='.'){
+                    direction[i]=' ';
+                }
+            }
+            std::istringstream in_(direction);
+            int vector[4];
+            in_ >> vector[0] >> vector[1] >> vector[2] >> vector[3];
+            if(in_){
+                for(int i=0;i<4;i++){
+                    if(vector[i]<0 || vector[i]>255){
+                        throw std::runtime_error("Invalid IP");
+                    }
+                    else{
+                        ip.bytes[i] = static_cast<uint8_t>(vector[i]);
+                    }
+                }
+            }
+            else{
+                throw std::runtime_error("Ip: wrong input format.");
+            }
+        }
+        else{
+            throw std::runtime_error("Ip: wrong input format.");
+        }
     //
     return in;
 }
